edf589ebabf919296d0b10e10006fe2a9cb3b935
Nariyuki Saito <toshincompos@gmail.com>
2023-12-31 18:36:38 +0900

    Implement create element

---
+++ didact.js
@@ -0,0 +1,44 @@
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+};
+
+const createTextElement = (text) => ({
+  type: "TEXT_ELEMENT",
+  props: {
+    nodeValue: text,
+    children: [],
+  },
+});
+
+const render = (element, parentDom) => {
+  console.log(element);
+  const { type, props } = element;
+  const isTextElement = type === "TEXT_ELEMENT";
+  const dom = isTextElement
+    ? document.createTextNode("")
+    : document.createElement(type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = props[name];
+    });
+
+  parentDom.appendChild(dom);
+
+  element.props.children.forEach((child) => render(child, dom));
+};
+
+const elem = <h1>hoge</h1>;
+
+const root = document.getElementById("root");
+render(elem, root);
